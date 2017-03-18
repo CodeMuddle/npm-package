142e2ded25cf5cfbc5e6ef55090c4f06e46b4c48
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-03-18 11:03:14 -0400

    Create a test for Typescript usage

---
+++ __tests__/index.ts
@@ -0,0 +1,17 @@
+// inside __tests__/index.ts
+describe('Exposed functions of Question Answering System', () => {
+    beforeEach(() => {
+        console.log('About to run the index.ts test');
+    });
+    // question: where is drake from?
+    // answers: 'Canada', 'Toronto, Canada'
+    test('the asked question has an answer', () => {
+        const question = 'where is drake from?';
+        let answers = ['Canada', 'Toronto', 'Toronto, Canada'];
+        var QASource = [{ question, answers }];
+        let sakwesakwe = new Sakwesakwe(QASource);
+        let predictedAnswer = sakwesakwe.ask(question);
+        let realAnswers = answers;
+        expect(realAnswers).toContain(predictedAnswer);
+    });
+});
